22dc1b0f6edf16a4e10e144b46fd9d34689d6607
nick <nickjobszh@gmail.com>
2020-08-10 17:10:30 +0200

    remove obsolete message

--- src/Exception/KafkaConsumerBuilderException.php
+++ src/Exception/KafkaConsumerBuilderException.php
@@ -9,6 +9,4 @@ class KafkaConsumerBuilderException extends \Exception
     public const NO_BROKER_EXCEPTION_MESSAGE = 'You need add at least one broker to connect to.';
     public const NO_TOPICS_EXCEPTION_MESSAGE = 'No topics defined to subscribe to.';
     public const UNSUPPORTED_CALLBACK_EXCEPTION_MESSAGE = 'The callback %s is not supported for %s';
-    public const TOPIC_SUBSCRIPTION_OFFSET_WITHOUT_PARTITIONS =
-        'If you define an offset for a topic subscription, partition(s) must be defined as well';
 }
