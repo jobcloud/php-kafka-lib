b02c90f8b819c020b64304f58bb49685ab5aa590
Nick <nickjobszh@gmail.com>
2021-05-16 16:27:24 +0200

    expose original error and exception (#62)

--- src/Exception/KafkaProducerTransactionAbortException.php
+++ src/Exception/KafkaProducerTransactionAbortException.php
@@ -7,5 +7,5 @@ namespace Jobcloud\Kafka\Exception;
 class KafkaProducerTransactionAbortException extends \Exception
 {
     public const TRANSACTION_REQUIRES_ABORT_EXCEPTION_MESSAGE =
-        'Produce failed. You need to abort your current transaction and start a new one';
+        'Produce failed. You need to abort your current transaction and start a new one (%s)';
 }
